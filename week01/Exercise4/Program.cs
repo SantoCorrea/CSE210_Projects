ac2e3070f5bd8edaa2a17634b0b917124c8917e3
Gabriel Guilherme Dos Santos Corrêa <santocorrea75@gmail.com>
2026-03-09 00:11:34 -0300

    finished exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,48 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+        while (userNumber != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);  
+            } 
+
+            int total = 0;
+            foreach (int number in numbers)
+            {
+                total += number;
+            }
+
+            Console.WriteLine($"The sum is: {total}");
+
+            float avarage = ((float)total) / numbers.Count;
+            Console.WriteLine($"The avarege is : {avarage}");
+
+            int max = numbers[0];
+
+            foreach (int number in numbers)
+            {
+                if (number > max)
+                {
+                    max = number;
+                }
+            }
+            Console.WriteLine($"The max is: {max}");
+
+
+        }
     }
 }
