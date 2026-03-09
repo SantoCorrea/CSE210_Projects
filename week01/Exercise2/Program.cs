fb7cc4246bebe41f1669c4179ef908f7df7456b6
Gabriel Guilherme Dos Santos Corrêa <santocorrea75@gmail.com>
2026-03-08 22:14:01 -0300

    finished exercise 02

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your percentage(number only, no symbols): ");
+        string userScore = Console.ReadLine();
+
+        int x = int.Parse(userScore);
+
+        if (x >= 90)
+        {
+            Console.WriteLine("Your grade is A.");
+        }
+
+        else if (x >= 80)
+        {
+            Console.WriteLine("Your grade is B.");
+        }
+
+        else if (x >= 70)
+        {
+            Console.WriteLine("Your grade is C.");
+        }
+
+        else if (x >= 60)
+        {
+            Console.WriteLine("Your grade is D.");
+        }
+
+        else
+        {
+            Console.WriteLine("Your grade is F.");
+        }
+
+        if (x >= 70)
+        {
+            Console.WriteLine("You have passed! congratulations.");
+        }
+
+        else
+        {
+            Console.WriteLine("You did not pass, sorry.");
+        }
+
+
+
     }
 }
