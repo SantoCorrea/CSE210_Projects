97f70417da41252383d5e0be0a790fd72fa22994
Gabriel Guilherme Dos Santos Corrêa <santocorrea75@gmail.com>
2026-03-08 23:09:38 -0300

    finished exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random () ;
+        int magicNumber = randomGenerator.Next(1,101);
+        int guess = -1;
+        int attempts = 0;
+
+        Console.WriteLine("Welcome to the 'WHAT IS THE MAGIC NUMBER GAME', good luck");
+
+        while (guess != magicNumber)
+        {
+            Console.Write("what is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+            attempts++;
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"You guessed {attempts} times");
+            }
+        }
+
+
     }
 }
